0fae47b3ff55f6ec779d05a81e75fdc499f0d2c2
programmer0101 <113084773+Yousuf9963@users.noreply.github.com>
2022-12-15 06:39:02 +0300

    uploaded yousuf.py

---
+++ get-website-ip.py
@@ -0,0 +1,33 @@
+print("[&] Coded By Yousuf Shafi'i Muhammad Junior Programmer.")
+
+print("")
+
+print("[-] Website muhammadabdirahman.wixsite.com/yousuf9963blog.")
+
+print("")
+print("[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
+
+
+print("I hope for you good future and i am willing that you will come high effort.")
+
+print("")
+
+
+import socket
+
+url = input("Enter Target Website :")
+
+print("Getting IP Address attacking........")
+print("Getting IP Address  0%")
+print("Getting IP Address  10%")
+print("Getting IP Address  20%")
+print("Getting IP Address  40%")
+print("Getting IP Address  60%")
+print("Getting IP Address  70%")
+print("Getting IP Address  80%")
+print("Getting IP Address  90%")
+print("Getting IP Address  100%")
+print("IP Address Founded Successsfully")
+
+print("IP Address is:",socket.gethostbyname(url))
+
