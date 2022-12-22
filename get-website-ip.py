7b0ac370a96db7e3f7dd55e053389f1e57ffc84c
programmer0101 <113084773+Yousuf9963@users.noreply.github.com>
2022-12-22 12:19:43 +0300

    Update get-website-ip.py

--- get-website-ip.py
+++ get-website-ip.py
@@ -1,38 +1,30 @@
-print(fore.GREEN + "[+] Tool Name:phone-num-info")
-print(fore.GREEN + "[+] Author:Yousuf Shafi'i Muhammad.(Junior Programmer")
-print(fore.GREEN + "[+] Version:1.3")
-print(fore.GREEN + "[+] Team:Junior Programmers")
-print(fore.GREEN + "[+] Github:https://github.com/Yousuf9963/phone-num-info")
-print(fore.GREEN + "[+] Follow me on Github: https://github.com/Yousuf9963")
-
-print("")
-
-print(fore.GREEN + "[-] Website muhammadabdirahman.wixsite.com/yousuf9963blog.")
-
-print("")
-print(fore.RED + "[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
-
-
-print(fore.GREEN + "I hope for you good future and i am willing that you will come high effort.")
-
-print("")
-
-
 import socket
 
-url = input(fore.GREEN + "Enter Target Website :")
+print("Version 1.3")
 
-print(fore.BLUE + "Getting IP Address attacking........")
-print(Fore.BLUE + "Getting IP Address  0%")
-print(fore.BLUE + "Getting IP Address  10%")
-print(fore.BLUE + "Getting IP Address  20%")
-print(fore.BLUE + "Getting IP Address  40%")
-print(fore.BLUE + "Getting IP Address  60%")
-print(fore.BLUE + "Getting IP Address  70%")
-print(fore.BLUE + "Getting IP Address  80%")
-print(fore.BLUE + "Getting IP Address  90%")
-print(fore.BLUE + "Getting IP Address  100%")
-print(fore.BLUE + "IP Address Founded Successsfully")
+print("[+] Tool Name:phone-num-info")
+print("[+] Author:Yousuf Shafi'i Muhammad.(Junior Programmer")
+print("[+] Version:1.3")
+print("[+] Team:Junior Programmers")
+print("[+] Github:https://github.com/Yousuf9963/phone-num-info")
+print("[+] Follow me on Github: https://github.com/Yousuf9963")
+print("[-] Website muhammadabdirahman.wixsite.com/yousuf9963blog.")
+print("[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
+print("I hope for you good future and i am willing that you will come high effort.")
 
-print(fore.GREEN + "IP Address is:",socket.gethostbyname(url))
+url = input"Enter Target Website :")
+
+print("Getting IP Address attacking........")
+print("Getting IP Address  0%")
+print("Getting IP Address  10%")
+print("Getting IP Address  20%")
+print("Getting IP Address  40%")
+print("Getting IP Address  60%")
+print("Getting IP Address  70%")
+print("Getting IP Address  80%")
+print("Getting IP Address  90%")
+print("Getting IP Address  100%")
+print"IP Address Founded Successsfully")
+
+print("IP Address is:",socket.gethostbyname(url))
 
