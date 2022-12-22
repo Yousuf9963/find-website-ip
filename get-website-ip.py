3c8e2fadcfbc5120703323195222d6e3a27b72a1
programmer0101 <113084773+Yousuf9963@users.noreply.github.com>
2022-12-22 11:44:58 +0300

    Update get-website-ip.py

--- get-website-ip.py
+++ get-website-ip.py
@@ -1,33 +1,38 @@
-print("[&] Coded By Yousuf Shafi'i Muhammad Junior Programmer.")
+print(fore.GREEN + "[+] Tool Name:phone-num-info")
+print(fore.GREEN + "[+] Author:Yousuf Shafi'i Muhammad.(Junior Programmer")
+print(fore.GREEN + "[+] Version:1.3")
+print(fore.GREEN + "[+] Team:Junior Programmers")
+print(fore.GREEN + "[+] Github:https://github.com/Yousuf9963/phone-num-info")
+print(fore.GREEN + "[+] Follow me on Github: https://github.com/Yousuf9963")
 
 print("")
 
-print("[-] Website muhammadabdirahman.wixsite.com/yousuf9963blog.")
+print(fore.GREEN + "[-] Website muhammadabdirahman.wixsite.com/yousuf9963blog.")
 
 print("")
-print("[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
+print("fore.RED[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
 
 
-print("I hope for you good future and i am willing that you will come high effort.")
+print(fore.GREEN"I hope for you good future and i am willing that you will come high effort.")
 
 print("")
 
 
 import socket
 
-url = input("Enter Target Website :")
+url = input(fore.GREEN"Enter Target Website :")
 
-print("Getting IP Address attacking........")
-print("Getting IP Address  0%")
-print("Getting IP Address  10%")
-print("Getting IP Address  20%")
-print("Getting IP Address  40%")
-print("Getting IP Address  60%")
-print("Getting IP Address  70%")
-print("Getting IP Address  80%")
-print("Getting IP Address  90%")
-print("Getting IP Address  100%")
-print("IP Address Founded Successsfully")
+print(fore.BLUE + "Getting IP Address attacking........")
+print(Fore.BLUE + "Getting IP Address  0%")
+print(fore.BLUE"Getting IP Address  10%")
+print(fore.BLUE + "Getting IP Address  20%")
+print(fore.BLUE + "Getting IP Address  40%")
+print(fore.BLUE + "Getting IP Address  60%")
+print(fore.BLUE + "Getting IP Address  70%")
+print(fore.BLUE + "Getting IP Address  80%")
+print(fore.BLUE + "Getting IP Address  90%")
+print(fore.BLUE + "Getting IP Address  100%")
+print(fore.BLUE + "IP Address Founded Successsfully")
 
-print("IP Address is:",socket.gethostbyname(url))
+print(fore.GREEN + "IP Address is:",socket.gethostbyname(url))
 
